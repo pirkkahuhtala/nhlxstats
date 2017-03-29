7db03ee67656254a91e62b6e7beec72bf3c16c19
Pirkka Huhtala <pirkka.huhtala@gmail.com>
2017-03-29 20:13:53 +0300

    Introduced player domain class.

---
+++ service/src/main/java/com/mediatuotantoph/nhlxstats/player/domain/Player.java
@@ -0,0 +1,10 @@
+package com.mediatuotantoph.nhlxstats.player.domain;
+
+/**
+ * Class for player which contain information about player profile.
+ * 
+ * @author Pirkka Huhtala
+ *
+ */
+public class Player {
+}
