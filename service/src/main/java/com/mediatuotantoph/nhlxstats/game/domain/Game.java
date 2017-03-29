ce50cb9c14d4275a0fa9cac83890f2b8fd49f1c3
Pirkka Huhtala <pirkka.huhtala@gmail.com>
2017-03-29 20:11:11 +0300

    Introduced game domain class.

---
+++ service/src/main/java/com/mediatuotantoph/nhlxstats/game/domain/Game.java
@@ -0,0 +1,10 @@
+package com.mediatuotantoph.nhlxstats.game.domain;
+
+/**
+ * Class for Game which contains info about the game score.
+ * 
+ * @author Pirkka Huhtala
+ *
+ */
+public class Game {
+}
