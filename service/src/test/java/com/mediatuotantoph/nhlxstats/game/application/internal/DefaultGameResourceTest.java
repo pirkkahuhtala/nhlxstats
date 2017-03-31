a9017ef80b8618b9c023dab0648180bbe8b1aa82
Pirkka Huhtala <pirkka.huhtala@gmail.com>
2017-03-31 11:51:20 +0300

    Implemented update, delete and find in DefaultGameResource.

--- service/src/test/java/com/mediatuotantoph/nhlxstats/game/application/internal/DefaultGameResourceTest.java
+++ service/src/test/java/com/mediatuotantoph/nhlxstats/game/application/internal/DefaultGameResourceTest.java
@@ -15,6 +15,7 @@ import com.mediatuotantoph.nhlxstats.game.application.GameDTO;
 import com.mediatuotantoph.nhlxstats.game.domain.model.Game;
 import com.mediatuotantoph.nhlxstats.game.domain.model.GameService;
 import com.mediatuotantoph.nhlxstats.game.domain.model.Side;
+import com.mediatuotantoph.nhlxstats.player.application.PlayerDTO;
 import com.mediatuotantoph.nhlxstats.player.domain.Player;
 import com.mediatuotantoph.nhlxstats.player.domain.PlayerService;
 
@@ -59,16 +60,66 @@ public class DefaultGameResourceTest {
     @Test
     public void testUpdate() throws Exception {
 
+        GameDTO gameDTO = mock(GameDTO.class);
+        Game game = mock(Game.class);
+        when(game.getHome()).thenReturn(mock(Side.class));
+        when(game.getVisitor()).thenReturn(mock(Side.class));
+        when(game.getHomePlayerId()).thenReturn(1);
+        when(game.getVisitorPlayerId()).thenReturn(2);
+        Player homePlayer = mock(Player.class);
+        when(playerService.find(1)).thenReturn(homePlayer);
+        Player visitorPlayer = mock(Player.class);
+        when(playerService.find(2)).thenReturn(visitorPlayer);
+        when(mapper.map(gameDTO, Game.class)).thenReturn(game);
+        GameDTO newGameDTO = mock(GameDTO.class);
+        when(mapper.map(game, GameDTO.class)).thenReturn(newGameDTO);
+        
+        gameResource.update(gameDTO);
+        
+        verify(game.getHome()).setPlayer(homePlayer);
+        verify(game.getVisitor()).setPlayer(visitorPlayer);
+        verify(gameService).update(game);
+        verify(mapper).map(newGameDTO, gameDTO);
+        
     }
 
     @Test
     public void testDelete() throws Exception {
-
+        
+        GameDTO gameDTO = mock(GameDTO.class);
+        Game game = mock(Game.class);
+        when(game.getHome()).thenReturn(mock(Side.class));
+        when(game.getVisitor()).thenReturn(mock(Side.class));
+        when(game.getHomePlayerId()).thenReturn(1);
+        when(game.getVisitorPlayerId()).thenReturn(2);
+        Player homePlayer = mock(Player.class);
+        when(playerService.find(1)).thenReturn(homePlayer);
+        Player visitorPlayer = mock(Player.class);
+        when(playerService.find(2)).thenReturn(visitorPlayer);
+        when(mapper.map(gameDTO, Game.class)).thenReturn(game);
+        GameDTO newGameDTO = mock(GameDTO.class);
+        when(mapper.map(game, GameDTO.class)).thenReturn(newGameDTO);
+        
+        gameResource.delete(gameDTO);
+        
+        verify(game.getHome()).setPlayer(homePlayer);
+        verify(game.getVisitor()).setPlayer(visitorPlayer);
+        verify(gameService).delete(game);
+        
     }
 
     @Test
     public void testFind() throws Exception {
 
+        PlayerDTO playerDTO = mock(PlayerDTO.class);
+        when(playerDTO.getId()).thenReturn(1);
+        Player player = mock(Player.class);
+        when(playerService.find(1)).thenReturn(player);
+        
+        gameResource.find(playerDTO);
+        
+        verify(gameService).find(player);
+        
     }
 
 }
