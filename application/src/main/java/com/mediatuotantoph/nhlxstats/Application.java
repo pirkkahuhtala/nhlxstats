93abb60b3e6ce7b6c3445522273a5c01cfe852ad
Pirkka Huhtala <pirkka.huhtala@gmail.com>
2017-04-03 17:46:14 +0300

    Removed comments

--- application/src/main/java/com/mediatuotantoph/nhlxstats/Application.java
+++ application/src/main/java/com/mediatuotantoph/nhlxstats/Application.java
@@ -3,12 +3,9 @@ package com.mediatuotantoph.nhlxstats;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication // same as @Configuration @EnableAutoConfiguration
-                       // @ComponentScan
+@SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
